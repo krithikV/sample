7289c4d7d304a26dde44c3e41225a79d4c25e895
krithikV <45972552+krithikV@users.noreply.github.com>
2021-06-05 14:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
-comand = ['wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh','sh install.sh','']
-for i in comand:
+comand = ['wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh','sh install.sh -i','apt update -y','apt upgrade -y','apt install curl','curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh','chmod +x metasploit.sh','./metasploit.sh']
+for i in comand
     os.system(i)
